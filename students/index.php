96d51e81eab718e61a572f3bc56891f6fba31d05
agsty44 <alexandiaingaming@gmail.com>
2025-10-02 23:26:15 +0100

    ADD: retrieve student info and recent grades to display on dashboard

--- students/index.php
+++ students/index.php
@@ -2,5 +2,30 @@
 // import header lib. this establishes db connections
 include($_SERVER['DOCUMENT_ROOT'] . 'header.php');
 
+is_user_in_right_area(1);                                                       // pass the access tier of this page into the parameters.
+$identifier = grab_user_id();
 
+// the above code is likely to be used in most instances of index.php for account verification.
+
+// generate a web page relevant to the student.
+
+// first, we need to withdraw any relevant data.
+
+// return info on the user
+$sql = 'SELECT `OtherNames`, `LastName`, `YearGroup` FROM `People` WHERE `PersonID` = ?';
+$query = $conn->prepare($sql);
+$query->bind_param('i', $identifier);
+$query->execute();
+$user_info = $query->get_result();
+$user_data_record = $user_info->fetch_assoc();
+$query->close();
+
+// return the most recent 3 grades
+$sql = 'SELECT Classes.`ClassName`, Grades.`Percentage`, Grades.`Comment` FROM Classes INNER JOIN Grades ON Classes.`ClassID` = Grades.`ClassID` WHERE grades.`StudentID` = ? ORDER BY Grades.Timestamp DESC LIMIT 3';
+$query = $conn->prepare($sql);
+$query->bind_param('i', $identifier);
+$query->execute();
+$grade_result = $query->get_result();
+$grade_record = $grade_result->fetch_assoc();
+$query->close();
 ?>
