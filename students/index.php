4d2c626bfcac805246b52d38084312c5042ade00
agsty44 <alexandiaingaming@gmail.com>
2025-10-04 14:01:14 +0100

    CHORE: stated intent of page at top of students/index.php

--- students/index.php
+++ students/index.php
@@ -5,6 +5,10 @@ include($_SERVER['DOCUMENT_ROOT'] . 'header.php');
 is_user_in_right_area(1);                                                       // pass the access tier of this page into the parameters.
 $identifier = grab_user_id();
 
+// what is this page meant to display?
+// this page will return the users name and year group as a kind of "title", so they know they are logged in correctly.
+// it will then display the users 3 most recent grades
+// it will also display their general timetable, and specifically their next upcoming lesson.
 // the above code is likely to be used in most instances of index.php for account verification.
 
 // generate a web page relevant to the student.
@@ -38,6 +42,8 @@ $grade_list = [];                                                           // d
 while ($row = $grade_result->fetch_assoc()) {                                   // iteratively add each grade to the array, this can be iterated later.
     $grade_list[] = $row;
 }
+
+// down here we need to use some form of concatenation to generate the html for the page, or we can use AJAX->js.
 
 $query->close();
 ?>
