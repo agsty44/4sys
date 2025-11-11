611fc4f54728d6740c41cc5f4ea078e9a333e192
agsty44 <170565596+agsty44@users.noreply.github.com>
2025-11-11 11:06:04 +0000

    ADD: greeting on student homepage

--- students/student_fetch_data.js
+++ students/student_fetch_data.js
@@ -1,9 +1,17 @@
 fetch('http://4sys.local:8080/api/student_data.php')
     .then(response => {
         if (!response.ok) throw new Error('Api failure');
-        return response.json
+        return response.json();
     })
     .then(data => {
+        const student_info = data.student_info;
+        const grades = data.grade_list;
+        const bus = data.bus_name;
+        // we need to write code for the timetable once refactored
+
+        // insert data
+        document.getElementById('greet-user').textContent = `Welcome back, ${student_info.OtherNames} ${student_info.LastName}.`;
+        document.getElementById('greet-subheading').textContent = `Year group: ${student_info.YearGroup}`;
         
     })
     .catch(error => {
