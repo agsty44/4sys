8e58ff8df43cfefe91504fca5fc1f6d2175d9f7e
agsty44 <170565596+agsty44@users.noreply.github.com>
2025-11-20 14:42:45 +0000

    FIX: links now point to the correct place

--- admin/student_search/student_search.php
+++ admin/student_search/student_search.php
@@ -5,3 +5,5 @@ include($_SERVER['DOCUMENT_ROOT'] . '/header.php');
 is_user_in_right_area(4);                                                       // pass the access tier of this page into the parameters.
 $identifier = grab_user_id();
 
+include('./student_search.html');
+?>
