77ab471363d6a880c7ad22e157d80ceab4b61043
agsty44 <170565596+agsty44@users.noreply.github.com>
2025-11-21 14:11:15 +0000

    FIX: minutes formatting when minutes < 10

--- admin/admin_fetch_data.js
+++ admin/admin_fetch_data.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/api/admin_data.php')
+    fetch('/api/admin_data.php')                                                // fetch query
         .then(response => {
             if (!response.ok) throw new Error('Api failure');
             return response.json();
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const minutes = now.getMinutes();
             const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
             const dayWord = weekdays[day];
-
+            const formatMinutes = minutes < 10 ? `0${minutes}` : minutes;       // force it to take proper formatting
             document.getElementById('greet-user').textContent = `Welcome back, ${adminInfo.OtherNames} ${adminInfo.LastName}. It is currently ${hours}:${minutes} on a ${dayWord}. Have a great day.`
         })
         .catch(error => {
