0fcc08513228a23d0f3a2d20289ba88829f3f402
agsty44 <170565596+agsty44@users.noreply.github.com>
2025-11-24 10:46:55 +0000

    FIX: identifier in view_student.php refers to the GET var not the account ID

--- admin/view_student/view_student.php
+++ admin/view_student/view_student.php
@@ -3,12 +3,13 @@
 include($_SERVER['DOCUMENT_ROOT'] . '/header.php');
 
 is_user_in_right_area(4);                                                       // pass the access tier of this page into the parameters.
-$identifier = grab_user_id();
 
 if (!isset($_GET['id'])) {
     header('Location: /admin/student_search/student_search.php');
     die();
 }
+
+$identifier = $_GET['id'];
 
 // SELECT COUNT TO CHECK USER EXISTS - VERY NEEDED!!!
 $sql = 'SELECT COUNT(*)
