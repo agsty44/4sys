fffc460974439f8a0e753ca6096111be644fdf49
agsty44 <170565596+agsty44@users.noreply.github.com>
2025-11-25 14:32:52 +0000

    FIX: api call dies without proper parameter

--- api/student_search.php
+++ api/student_search.php
@@ -5,6 +5,10 @@ include($_SERVER['DOCUMENT_ROOT'] . '/header.php');
 is_user_in_right_area(4);                                                       // pass the access tier of this page into the parameters.
 $identifier = grab_user_id();
 
+if (!isset($_GET['query'])) {
+    die();                                                                      // if no query is given, just exit     
+}
+
 $search_query = $_GET['query'];
 
 $sql = 'SELECT `PersonID`, `OtherNames`, `LastName`
