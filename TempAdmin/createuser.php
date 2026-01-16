074673f0c492108cc28c3b835b1ee21ad9e02920
agsty44 <170565596+agsty44@users.noreply.github.com>
2026-01-16 19:04:31 +0000

    FIX: temp admin page sends values to username instead of email

--- TempAdmin/createuser.php
+++ TempAdmin/createuser.php
@@ -5,7 +5,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/header.php');
 $email = $_POST['email'];
 $pass = $_POST['pass'];
 
-$sql = 'INSERT INTO `People` (`Email`, `PassHash`) VALUES (?, ?)';
+$sql = 'INSERT INTO `People` (`Username`, `PassHash`) VALUES (?, ?)';
 
 $pass = password_hash($pass, PASSWORD_DEFAULT);
 
