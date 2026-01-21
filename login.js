048c2389a3e694426fed06e5155e3ebdc96c6645
agsty44 <170565596+agsty44@users.noreply.github.com>
2026-01-21 11:12:43 +0000

    FIX: wrapped API call in an event listener for the form submit key and gave ids to both login fields

--- login.js
+++ login.js
@@ -1,29 +1,35 @@
-function login() {
-    fetch('/api/login.php', {
-        method: 'POST',
-        body: new URLSearchParams({
-            'username': document.querySelector('input[name="username"]').value,
-            'pass': document.querySelector('input[name="pass"]').value
-        })
-    })
-    .then(response => {
-        if (!response.ok) throw new Error('Login failed');
-        return response.json();
-    })
-    .then(data => {
-        if (data.result === 'failure') { // login failed, reload login
-            window.location.replace('/');
-        }
-        else {
-            roleID = data.access_tier;
-            let location = '';
-            if (roleID === 0) location = '/bus';
-            else if (roleID === 1) location = '/student';
-            else if (roleID === 2) location = '/parent';
-            else if (roleID === 3) location = '/teacher';
-            else if (roleID === 4) location = '/admin';
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login_form');
 
-            window.location.replace(location);
-        }
-    })
-}
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault(); // Prevent the default form submission
+
+        fetch('/api/login.php', {
+            method: 'POST',
+            body: new URLSearchParams({
+                'username': getElementById('username').value,
+                'pass': getElementById('pass').value
+            })
+        })
+        .then(response => {
+            if (!response.ok) throw new Error('Login failed');
+            return response.json();
+        })
+        .then(data => {
+            if (data.result === 'failure') { // login failed, reload login
+                window.location.replace('/');
+            }
+            else {
+                roleID = data.access_tier;
+                let location = '';
+                if (roleID === 0) location = '/bus';
+                else if (roleID === 1) location = '/student';
+                else if (roleID === 2) location = '/parent';
+                else if (roleID === 3) location = '/teacher';
+                else if (roleID === 4) location = '/admin';
+
+                window.location.replace(location);
+            }
+        })
+    });
+});
