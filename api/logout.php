5602b8a4c735ba68399fe02a32ee2efae7590de8
agsty44 <170565596+agsty44@users.noreply.github.com>
2026-02-12 09:51:56 +0000

    ADD: api/logout.php now returns 'result: failure' if user was not logged in

--- api/logout.php
+++ api/logout.php
@@ -1,8 +1,20 @@
 <?php
-setcookie('auth_token', '', time() - 1, '/');                                   // delete token cookie
 header('Content-Type: application/json');                                       // set header to be json
+
+if (!isset($_COOKIE['auth_token'])) {
+    $json = json_encode([
+        'result' => 'failure'
+    ]);
+    echo($json);
+    die();
+}
+
+setcookie('auth_token', '', time() - 1, '/');                                   // delete token cookie
+
 $json = json_encode([
     'result' => 'success'
 ]);
+
 echo($json);
+die();
 ?>
